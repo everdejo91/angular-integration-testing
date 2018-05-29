51a6a8f240db2b3d5cbdd721612181eec27f95a5
everdejo91 <39573542+everdejo91@users.noreply.github.com>
2018-05-29 18:47:23 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { HomeComponent } from './home/home.component';
+//import { HomeComponent } from './home/home.component';
 import { VoterComponent } from './voter/voter.component';
 import { TodosComponent } from './todos/todos.component';
 import { UsersComponent } from './users/users.component'; 
